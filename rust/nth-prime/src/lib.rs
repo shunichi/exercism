8044d78352d12e6df53ed95daa7c1cd8a43c630f
Shunichi Ikegami <sike.tm@gmail.com>
2021-04-26 00:46:11 +0900

    solve rust/nth-prime

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,3 +1,18 @@
+use std::convert::TryFrom;
+
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut primes: Vec<u32> = vec![];
+    let mut num = 2;
+    let n_usize = usize::try_from(n).unwrap();
+    while primes.len() <= n_usize {
+        if not_divisible(num, &primes) {
+            primes.push(num);
+        }
+        num += 1;
+    }
+    *primes.last().unwrap()
+}
+
+fn not_divisible(n: u32, primes: &Vec<u32>) -> bool {
+    primes.iter().all(|p| n % p != 0)
 }
