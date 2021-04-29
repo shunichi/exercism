7ea59c3798f6cffee097d8f2d4c040e6b4488ffb
Shunichi Ikegami <sike.tm@gmail.com>
2021-04-30 01:10:49 +0900

    solve go/hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,20 @@
+// Package hamming implements the Hamming Distance calculation
 package hamming
 
+import (
+	"errors"
+)
+
+// Distance calculates the Hamming Distance between two strings
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return 0, errors.New("different length")
+	}
+	distance := 0
+	for i, c := range []byte(a) {
+		if c != b[i] {
+			distance++
+		}
+	}
+	return distance, nil
 }
