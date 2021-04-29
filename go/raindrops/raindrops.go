9614c29c07d89295d914c208fed34fdc367fe1c6
Shunichi Ikegami <sike.tm@gmail.com>
2021-04-30 01:38:48 +0900

    solve go/raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,1 +1,24 @@
+// Package raindrops implements integer to raindrop string conversion
 package raindrops
+
+import (
+	"strconv"
+)
+
+// Convert converts integer to raindrop string
+func Convert(t int) string {
+	result := ""
+	if t%3 == 0 {
+		result += "Pling"
+	}
+	if t%5 == 0 {
+		result += "Plang"
+	}
+	if t%7 == 0 {
+		result += "Plong"
+	}
+	if result == "" {
+		return strconv.Itoa(t)
+	}
+	return result
+}
