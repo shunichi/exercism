9663971372198c5d0748013aa7fedb2aba1f73ed
Shunichi Ikegami <sike.tm@gmail.com>
2021-05-06 14:26:52 +0900

    solve go/isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,1 +1,21 @@
+// Package isogram implements isogram checker
 package isogram
+
+import (
+	"strings"
+)
+
+// IsIsogram check if stinng is isogram
+func IsIsogram(s string) bool {
+	letterMap := make(map[rune]bool)
+	for _, c := range strings.ToLower(s) {
+		if c == '-' || c == ' ' {
+			continue
+		}
+		if letterMap[c] {
+			return false
+		}
+		letterMap[c] = true
+	}
+	return true
+}
