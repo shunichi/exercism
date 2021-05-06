e873671d945268e06afc14fbc12f69edde502f27
Shunichi Ikegami <sike.tm@gmail.com>
2021-05-06 14:05:06 +0900

    solve rust/space-age

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,21 +1,24 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
+use conv::ValueFrom;
 
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    seconds: u64,
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {}", s)
+        Duration { seconds: s }
     }
 }
 
+const EARTH_YEAR_SECONDS: f64 = 31557600.0;
+
 pub trait Planet {
+    const ORBITAL_PERIOD: f64 = 1.0;
+
     fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
+        let seconds = f64::value_from(d.seconds).unwrap();
+        seconds / (Self::ORBITAL_PERIOD * EARTH_YEAR_SECONDS)
     }
 }
 
@@ -28,11 +31,27 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    const ORBITAL_PERIOD: f64 = 0.2408467;
+}
+impl Planet for Venus {
+    const ORBITAL_PERIOD: f64 = 0.61519726;
+}
+impl Planet for Earth {
+    const ORBITAL_PERIOD: f64 = 1.0;
+}
+impl Planet for Mars {
+    const ORBITAL_PERIOD: f64 = 1.8808158;
+}
+impl Planet for Jupiter {
+    const ORBITAL_PERIOD: f64 = 11.862615;
+}
+impl Planet for Saturn {
+    const ORBITAL_PERIOD: f64 = 29.447498;
+}
+impl Planet for Uranus {
+    const ORBITAL_PERIOD: f64 = 84.016846;
+}
+impl Planet for Neptune {
+    const ORBITAL_PERIOD: f64 = 164.79132;
+}
