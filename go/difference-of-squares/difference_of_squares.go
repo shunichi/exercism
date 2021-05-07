a4a9f5ccecec62727aa103e2136016dc325dd70e
Shunichi Ikegami <sike.tm@gmail.com>
2021-05-07 09:46:24 +0900

    solve go/difference-of-squares

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,1 +1,25 @@
+// Package diffsquares calculates the difference between the square of the sum and the sum of the squares of the first N natural numbers
 package diffsquares
+
+// SquareOfSum calculates the square of the sum of the first N natural numbers
+func SquareOfSum(n int) int {
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+	return sum * sum
+}
+
+// SumOfSquares calculates the sum of the square of the first N natural numbers
+func SumOfSquares(n int) int {
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i * i
+	}
+	return sum
+}
+
+// Difference calculates the difference between the square of the sum and the sum of the squares of the first N natural numbers
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
